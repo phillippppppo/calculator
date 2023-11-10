065994e420a162f1ac76649fb5d9e0fc9b5d1f6e
phill <bo.ferch@web.de>
2023-11-10 17:53:48 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client"; // Import from "react-dom/client" instead of "react-dom"
 import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
-const { render } = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
-render(<App />);
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
