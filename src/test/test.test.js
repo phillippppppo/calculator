c1e55094c9e91df214cd13ea4a5d25a2ef6e1f32
phill <bo.ferch@web.de>
2023-12-11 14:52:11 +0100

    test

--- src/test/test.test.js
+++ src/test/test.test.js
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
 import App from "../component/App";
 import '@testing-library/jest-dom';
-import { render, screen } from "@testing-library/react";
+
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
