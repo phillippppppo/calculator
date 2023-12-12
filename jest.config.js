6df033c5c6a93fd410a22ee2a48c76bad9b9c963
phill <bo.ferch@web.de>
2023-12-12 19:57:23 +0100

    changes

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "\\.(css|less|scss)$": "identity-obj-proxy",
   },
   moduleDirectories: ["node_modules", "<rootDir>/src"],
+  testEnvironment: "jest-environment-jsdom",
   globals: {
     "IS_REACT_ACT_ENVIRONMENT": true,
   },
