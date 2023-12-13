7f7e132325e66ea82eeae6f51fbb7411a4977f84
phill <bo.ferch@web.de>
2023-12-13 12:26:17 +0100

    changes

--- src/test/test.test.js
+++ src/test/test.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import chai from "chai";
 import { act } from 'react-dom/test-utils';
-import App from "../component/App";
 import '@testing-library/jest-dom';
-
+import '@testing-library/react'
+import App from "../component/App";
+import Button from "../component/Button";
+import Display from "../component/Display";
+import ButtonPanel from "../component/ButtonPanel";
+import operate from "../logic/operate";
+import calculate from "../logic/calculate";
+import isNumber from "../logic/isNumber";
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
@@ -12,8 +19,4 @@ test("renders without crashing", () => {
     root.render(<App />);
   });
 });
-
-
-
-
 
